63b2d8573f74ea751791b41a877ea188c7d697cb
Float3 <86748455+Float3@users.noreply.github.com>
2021-12-15 15:01:45 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # psd-translate
 
-Translate layer names in .psd files from japanese to english
+Translate layer names in .psd files from jp/cn/kr or specifically anything in this unicode range [\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]
+
+edit [L22](../blob/master/translate.py#22) to change target language
 
 edit dropPSDhere.bat and change "yourPythonPathHere" to your python install-path to drop multiple .psd files onto the .bat file and translate all of them at once
 otherwise use 
@@ -11,3 +13,4 @@ Python -m python [relative/path/to/translate.py] [input_file_path1] [input_file_
 
 if a [input_file_path] doesn't end in .psd it will be ignored.
 you can specify multiple
+
--- translate.py
+++ translate.py
@@ -4,36 +4,38 @@ import sys
 from external_tools.google_trans_new.google_trans_new import google_translator
 from external_tools.psd_tools import PSDImage
 
-#usage: Python -m python translate.py [input_file_path1] [input_file_path2] ...
+# usage: Python -m python translate.py [input_file_path1] [input_file_path2] ...
 #
-# if a [input_file_path] doesn't end in .psd it will be ignored 
-# you can specify as many as you want 
+# if a [input_file_path] doesn't end in .psd it will be ignored
+# you can specify as many as you want
 
 if len(sys.argv) < 2:
-    print ('No input file specified.')
+    print('No input file specified.')
     input()
     sys.exit()
 
 
 regex = u'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+'
 
-translator = google_translator()  
+translator = google_translator()
 
+lang = 'en' # change this line to adjust output language
 
 for i in sys.argv:
 
     if i.endswith(".psd"):
-    
+
         psd = PSDImage.open(i)
 
         for layer in psd:
             print(layer.name)
             if len(re.findall(regex, layer.name)) > 0:
-                layer.name = translator.translate(layer.name,lang_tgt='en')  
-            
+                layer.name = translator.translate(layer.name, lang)
+
             if layer.is_group():
                 for childLayer in layer:
                     if len(re.findall(regex, childLayer.name)) > 0:
-                        childLayer.name = translator.translate(childLayer.name,lang_tgt='en')
+                        childLayer.name = translator.translate(
+                            childLayer.name, lang)
 
         psd.save(i)
