6efe0aa4c67958c5cce51aa14cd4e1a134847ea1
Float3 <86748455+Float3@users.noreply.github.com>
2021-12-15 14:23:29 +0100

    first commit

---
+++ translate.py
@@ -0,0 +1,36 @@
+﻿import re
+import sys
+
+from external_tools.google_trans_new.google_trans_new import google_translator
+from external_tools.psd_tools import PSDImage
+
+##usage Python -m python translate.py input_file_path1 input_file_path2
+
+if len(sys.argv) < 2:
+    print ('No input file specified.')
+    input()
+    sys.exit()
+
+
+regex = u'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+'
+
+translator = google_translator()  
+
+
+for i in sys.argv:
+
+    if i.endswith(".psd"):
+    
+        psd = PSDImage.open(i)
+
+        for layer in psd:
+            print(layer.name)
+            if len(re.findall(regex, layer.name)) > 0:
+                layer.name = translator.translate(layer.name,lang_tgt='en')  
+            
+            if layer.is_group():
+                for childLayer in layer:
+                    if len(re.findall(regex, childLayer.name)) > 0:
+                        childLayer.name = translator.translate(childLayer.name,lang_tgt='en')
+
+        psd.save(i)
