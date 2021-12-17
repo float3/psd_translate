d43109be93d6b63c52c76b4db3abd066f552a272
Float3 <86748455+Float3@users.noreply.github.com>
2021-12-17 20:21:12 +0100

    added test

---
+++ psd-translate-test.py
@@ -0,0 +1,25 @@
+﻿import re
+import os
+import sys
+from external_tools.google_trans_new.google_trans_new import google_translator
+from os import listdir
+from os.path import isfile, join
+
+jp = u'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u3400-\u4dbf]+'
+
+cn = u'[\u3400-\u4DB5\u6300-\u77FF\u7800-\u8CFF\u8D00-\u9FCC\u2e80-\u2fd5\u3190-\u319f\u3400-\u4DBF\u4E00-\u9FCC\uF900-\uFAAD\u20000-\u215FF\u21600-\u230FF\u23100-\u245FF\u24600-\u260FF\u26100-\u275FF\u27600-\u290FF\u29100-\u2A6DF\u2A700-\u2B734\u2B740-\u2B81D]+'
+
+kr = u'[\uac00-\ud7a3]+'
+
+regex = u'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u3400-\u4dbf\u3400-\u4DB5\u6300-\u77FF\u7800-\u8CFF\u8D00-\u9FCC\u2e80-\u2fd5\u3190-\u319f\u3400-\u4DBF\u4E00-\u9FCC\uF900-\uFAAD\u20000-\u215FF\u21600-\u230FF\u23100-\u245FF\u24600-\u260FF\u26100-\u275FF\u27600-\u290FF\u29100-\u2A6DF\u2A700-\u2B734\u2B740-\u2B81D\uac00-\ud7a3]+'
+
+translator = google_translator()
+lang = 'en'
+
+with open('tests/test1.txt', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        if len(re.findall(regex, line)) > 0:
+            print(translator.translate(line, lang))
+        else:
+            print(line)
